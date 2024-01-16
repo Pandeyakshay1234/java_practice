cd455de9634a13ec2d1c471ee8a646fd0b5e0b9e
Akshay <akshaypandey.study@gmail.com>
2024-01-16 23:24:29 +0530

    Merge Sort

---
+++ mergesort.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+
+public class mergesort {
+    public static int[] sort(int []a , int []b)
+    {  int []  res = new int[a.length+b.length];
+        int i =0;
+        int j =0;
+        int k =0;
+        while(i<a.length && j<b.length)
+        {
+            if(a[i]<b[j])
+            {
+              res[k] = a[i];
+              k++;
+              i++;
+            }
+            {
+                res[k] = b[j];
+                k++;
+                j++;
+            }
+        }
+        while(i<a.length)
+        {
+            res[k]=a[i];
+            k++;
+            i++;
+        }
+       while(j<b.length)
+       {
+        res[k] = b[j];
+        k++;
+        j++;
+       }
+       return res ;
+    }
+ 
+    public static void print(int[] arr)
+    {
+        for(int i =0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first array size and elements");
+        int n = sc.nextInt();
+        int []  a = new int[n];
+        for(int i=0;i<a.length;i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        System.out.println("enter second array size and elements");
+    int m = sc.nextInt();
+    int [] b = new int[m];
+    for(int i=0;i<b.length;i++)
+    {
+        b[i] = sc.nextInt();
+    }
+
+    int [] mergearray =  sort(a, b);
+    print(mergearray);
+}
+}
+
+
+
